62d4fd9bc73a2013705b61435f16f4de25d3ee24
Matthew Brooks <31455321+Sirbigbeard@users.noreply.github.com>
2022-05-28 00:20:28 -0700

    Movement and Camera
    Player movement method finished, game over method started, camera fixed.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float horizontalInput;
+    private float verticalInput;
+    private float speed = 15;
+    private Rigidbody playerRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RegisterMovement();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != "projectile")
+        {
+            GameOver();
+            Destroy(other);
+        }
+    }
+    private void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+    private void RegisterMovement()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+        transform.Translate(-Vector3.left * Time.deltaTime * horizontalInput * speed);
     }
 }
